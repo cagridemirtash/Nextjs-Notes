9247f5b9cbcab6472675580b2591b606ca30384c
cagridemirtash <cagridemirtash@gmail.com>
2022-12-28 12:21:34 +0300

    Dynamic number with Function
    Add some dynamic process for using easily

--- introduction-hooks/src/App.jsx
+++ introduction-hooks/src/App.jsx
@@ -5,12 +5,35 @@ import { useState } from 'react';
 function App() {
   const [state, setState] = useState(0)
 
+  function increment(){
+    setState(state + 1)
+  }
+
+  function decrement(){
+    if(state === 0){
+      console.log("Daha fazla azaltamazsınız")
+      return;
+    } 
+    setState(state - 1)
+  }
+  //TODO : Fix with input data 
+  function multiply(){
+    setState(state * 5)
+  }
+
+  function reset(){
+    setState(1)
+  }
+
   return (
     <>
       <h1>Hello This is a increment count with useState hooks</h1>
       <h1>{state > 0 && state}</h1>
-      <button onClick={() => setState(state + 1)}>Arttir</button>
-      <button onClick={() => setState(state - 1)}>Azalt</button>
+      <button onClick={increment}>Arttir</button>
+      <button onClick={decrement}>Azalt</button>
+      <button onClick={reset}>Sıfırla</button>
+      <hr />
+      <button onClick={multiply}>Çarp</button>
     </>
   );
 }
